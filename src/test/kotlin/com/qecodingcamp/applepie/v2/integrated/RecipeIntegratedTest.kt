a68a4d86afe844bdc9cd7c923fb839a37db1bfe5
KseniaBudlianska <ksenia.titova@n26.com>
2022-09-23 15:41:30 +0200

    Cover controller tests

--- src/test/kotlin/com/qecodingcamp/applepie/v2/integrated/RecipeIntegratedTest.kt
+++ src/test/kotlin/com/qecodingcamp/applepie/v2/integrated/RecipeIntegratedTest.kt
@@ -1,11 +1,13 @@
 package com.qecodingcamp.applepie.v2.integrated
 
+import com.qecodingcamp.applepie.domain.RecipeCreationDto
 import io.restassured.RestAssured
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.server.LocalServerPort
 import java.io.File
+import java.util.UUID.randomUUID
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RecipeIntegratedTest {
@@ -31,6 +33,42 @@ class RecipeIntegratedTest {
             .statusCode(200)
     }
 
-    // cover all functions, check only status code
-    
+    @Test
+    fun getRecipesByName() {
+        RestAssured.given()
+            .`when`()
+            .get("/find/v2?name=test")
+            .then()
+            .statusCode(200)
+    }
+
+    @Test
+    fun getRecipesById() {
+        RestAssured.given()
+            .`when`()
+            .get("/find/v3?id=${randomUUID()}")
+            .then()
+            .statusCode(200)
+    }
+
+    @Test
+    fun createRecipe() {
+        val recipe = RecipeCreationDto(recipeName = "name")
+        RestAssured.given()
+            .`when`()
+            .contentType("application/json")
+            .body(recipe)
+            .post()
+            .then()
+            .statusCode(200)
+    }
+
+    @Test
+    fun deleteRecipe() {
+        RestAssured.given()
+            .`when`()
+            .delete("/${randomUUID()}")
+            .then()
+            .statusCode(200)
+    }
 }
